caacac5b2ae843c651311f39b3760bc682200dab
ismaeleision <ismaeleision@gmail.com>
2022-02-20 19:11:07 +0100

    20/02/2022

---
+++ database/migrations/2022_02_20_175005_create_eventos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->date('fecha');
+            $table->string('descripcion');
+            $table->string('ciudad');
+            $table->string('direccion');
+            $table->integer('aforomax');
+            $table->string('tipo');
+            $table->integer('nummaxentradas');
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('creador_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreign('creador_id')->references('id')->on('creadores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+};
