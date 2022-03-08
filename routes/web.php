50d6f2c497304dc26817249e7b76bbe42b5fbafe
ismaeleision <ismaeleision@gmail.com>
2022-03-08 17:36:03 +0100

    Evento

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::middleware(['auth'])->group(function () {
+    //Rutas asociadas al controlador resource CitaController
+    //GET 	/eventos 	index 	eventos.index
+    //GET 	/eventos/create 	create 	eventos.create
+    //POST 	/eventos 	store 	eventos.store
+    //GET 	/eventos/{cita} 	show 	eventos.show
+    //GET 	/eventos/{cita}/edit 	edit 	eventos.edit
+    //PUT/PATCH 	/eventos/{cita} 	update 	eventos.update
+    //DELETE 	/eventos/{cita} 	destroy 	eventos.destroy
+    Route::resource('eventos', EventoController::class);
+});
+
+
 require __DIR__ . '/auth.php';
