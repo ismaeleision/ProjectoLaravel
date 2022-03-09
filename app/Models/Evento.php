571f0ed2b6abd96b7ebb75c45509a0368e29c756
ismaeleision <ismaeleision@gmail.com>
2022-03-09 21:02:19 +0100

    Tabla Evento

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Categoria;
+use App\Models\User;
 
 class Evento extends Model
 {
@@ -13,5 +14,9 @@ class Evento extends Model
     public function categoria()
     {
         return $this->belongsTo(Categoria::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- resources/views/evento.blade.php
+++ resources/views/evento.blade.php
@@ -18,22 +18,46 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 text-4xl m-5">
-                    Eventeame esta
-                    <div class="relative">
-                        @foreach($eventos as $evento)
-                        <a href="/eventos/{{$evento->id}}">
-                            <div class="border border-black-500 w-fit flex items-center">
-                                <img src="{{$evento->imagen}}" class="w-4/12">
-                                <h3 class="m-5 text-2xl">{{$evento->nombre}}</h3>
-                                <h4 class="m-4 text-xl">{{$evento->categoria->nombre}}</h4>
-                                <p>{{$evento->descripcion}}</p>
-                            </div>
-                        </a>
-                        @endforeach
-                    </div>
+                <div class="p-6 bg-white border-b border-gray-200 m-5">
+                    <table class="min-w-full border-collapse block md:table">
+                        <thead class="block md:table-header-group">
+                            <tr class="border border-green-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+                                <th class="bg-green-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell"></th>
+                                <th class="bg-green-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Nombre</th>
+                                <th class="bg-green-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Fecha</th>
+                                <th class="bg-green-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Ciudad</th>
+                                <th class="bg-green-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Aforo Max</th>
+                                <th class="bg-green-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Organizador</th>
+                                <th class="bg-green-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Categoria</th>
+                                <th class="bg-green-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Tipo</th>
+                                <th class="bg-green-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody class="block md:table-row-group">
+                            @foreach($eventos as $evento)
+                            <a href="/eventos/{{$evento->id}}">
+                                <tr class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><img src="{{$evento->imagen}}"></td>
+                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->nombre}}</td>
+                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->fecha}}</td>
+                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->ciudad}}</td>
+                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->aforomax}}</td>
+                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->user->nombre}}</td>
+                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->categoria->nombre}}</td>
+                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->tipo}}</td>
+                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                        <span class="inline-block w-1/3 md:hidden font-bold">Acciones</span>
+                                        <a href="/eventos/{{ $evento->id }}/edit" data-method='edir'><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Editar</button></a>
+                                        <a href="/eventos/{{$evento->id}}" data-method='delete'><button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Eliminar</button></a>
+                                    </td>
+                                </tr>
+                            </a>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </x-app-layout>
