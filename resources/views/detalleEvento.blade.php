20925247117ea4ec021e3d9371e65998cfc67a48
ismaeleision <ismaeleision@gmail.com>
2022-03-10 13:02:24 +0100

    Detalle Pedido Casi Terminado

--- resources/views/detalleEvento.blade.php
+++ resources/views/detalleEvento.blade.php
@@ -3,55 +3,102 @@
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
       {{ __('Detalle Evento') }}--{{$evento->nombre}}
       @auth
+      @if(Auth::user()->id != $evento->user->id)
       <a href="*">
         <x-button class="ml-3">
           {{ __('Inscribete') }}
         </x-button>
       </a>
+      @endif
       @endauth
     </h2>
   </x-slot>
 
-  <div class=" rounded overflow-hidden border w-full lg:w-6/12 md:w-6/12 bg-white mx-3 md:mx-0 lg:mx-0">
-    <div class="w-full flex justify-between p-3">
-      <div class="flex">
-        <div class="rounded-full h-8 w-8 bg-gray-500 flex items-center justify-center overflow-hidden">
-          <img src="https://avatars0.githubusercontent.com/u/38799309?v=4" alt="profilepic">
-        </div>
-        <span class="pt-1 ml-2 font-bold text-sm">braydoncoyer</span>
-      </div>
+  <div class=" rounded overflow-hidden border w-full  bg-white mx-3 md:mx-0 lg:mx-0">
+    <div class="w-full flex justify-between p-3 items-center">
       <span class="px-2 hover:bg-gray-300 cursor-pointer rounded"><i class="fas fa-ellipsis-h pt-2 text-lg"></i></span>
     </div>
-    <img class="w-full bg-cover" src="https://3.bp.blogspot.com/-Chu20FDi9Ek/WoOD-ehQ29I/AAAAAAAAK7U/mc4CAiTYOY8VzOFzBKdR52aLRiyjqu0MwCLcBGAs/s1600/DSC04596%2B%25282%2529.JPG">
-    <div class="px-3 pb-2">
-      <div class="pt-2">
-        <i class="far fa-heart cursor-pointer"></i>
-        <span class="text-sm text-gray-400 font-medium">12 likes</span>
-      </div>
+    <div class="h-6/12 w-9/12">
+      <img class="w-full bg-cover" src="{{$evento->imagen}}">
+    </div>
+    <div class="px-3 pb-2 w-fit flex items-center bg-grey-300 border-black-500 mt-4">
+      <!--Nombre-->
       <div class="pt-1">
         <div class="mb-2 text-sm">
-          <span class="font-medium mr-2">braydoncoyer</span> Lord of the Rings is my favorite film-series. One day I'll make my way to New Zealand to visit the Hobbiton set!
+          <span class="font-medium mr-2">Nombre</span>
+          <h3 class="m-5 text-2xl">{{$evento->nombre}}</h3>
         </div>
       </div>
-      <div class="text-sm mb-2 text-gray-400 cursor-pointer font-medium">View all 14 comments</div>
-      <div class="mb-2">
-        <div class="mb-2 text-sm">
-          <span class="font-medium mr-2">razzle_dazzle</span> Dude! How cool! I went to New Zealand last summer and had a blast taking the tour! So much to see! Make sure you bring a good camera when you go!
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 bg-white border-b border-gray-200 text-4xl m-5">
-          <div class="border border-black-500 w-fit flex items-center">
-            <img src="{{$evento->imagen}}" class="w-4/12">
-            <h3 class="m-5 text-2xl">{{$evento->nombre}}</h3>
-            <h4 class="m-4 text-xl">{{$evento->categoria->nombre}}</h4>
-            <p class="ml-3">{{$evento->descripcion}}</p>
-          </div>
+      <!--Descripcion-->
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Descripcion</span>
+          <h3 class="m-5 text-2xl">{{$evento->descripcion}}</h3>
+        </div>
+      </div>
+
+      <!--Fecha-->
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Fecha</span>
+          <h3 class="m-5 text-2xl">{{$evento->fecha}}</h3>
+        </div>
+      </div>
+
+      <!--Ciudad-->
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Ciudad</span>
+          <h3 class="m-5 text-2xl">{{$evento->ciudad}}</h3>
+        </div>
+      </div>
+
+      <!--Direccion-->
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Direccion</span>
+          <h3 class="m-5 text-2xl">{{$evento->direccion}}</h3>
+        </div>
+      </div>
+
+      <!--Aforo-->
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Aforo Maximo</span>
+          <h3 class="m-5 text-2xl">{{$evento->aforomax}}</h3>
+        </div>
+      </div>
+
+      <!--Entradas-->
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Entradas Disponibles</span>
+          <h3 class="m-5 text-2xl">{{$evento->nummaxentradas}}</h3>
+        </div>
+      </div>
+
+      <!--Tipo-->
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Tipo</span>
+          <h3 class="m-5 text-2xl">{{$evento->tipo}}</h3>
+        </div>
+      </div>
+
+      <!--Categoria-->
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Categoria</span>
+          <h3 class="m-5 text-2xl">{{$evento->categoria->nombre}}</h3>
+        </div>
+      </div>
+
+      <!--Organizador-->
+      <div class="pt-1">
+        <div class="mb-2 text-sm">
+          <span class="font-medium mr-2">Organizador</span>
+          <h3 class="m-5 text-2xl">{{$evento->user->nombre}}</h3>
         </div>
       </div>
     </div>
--- resources/views/evento.blade.php
+++ resources/views/evento.blade.php
@@ -35,23 +35,29 @@
                         </thead>
                         <tbody class="block md:table-row-group">
                             @foreach($eventos as $evento)
-                            <a href="/eventos/{{$evento->id}}">
-                                <tr class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
-                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><img src="{{$evento->imagen}}"></td>
-                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->nombre}}</td>
-                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->fecha}}</td>
-                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->ciudad}}</td>
-                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->aforomax}}</td>
-                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->user->nombre}}</td>
-                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->categoria->nombre}}</td>
-                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->tipo}}</td>
-                                    <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-                                        <span class="inline-block w-1/3 md:hidden font-bold">Acciones</span>
-                                        <a href="/eventos/{{ $evento->id }}/edit" data-method='edir'><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Editar</button></a>
-                                        <a href="/eventos/{{$evento->id}}" data-method='delete'><button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Eliminar</button></a>
-                                    </td>
-                                </tr>
-                            </a>
+
+                            <tr class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><img src="{{$evento->imagen}}"></td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->nombre}}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->fecha}}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->ciudad}}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->aforomax}}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->user->nombre}}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->categoria->nombre}}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{$evento->tipo}}</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                    <a href="/eventos/{{$evento->id}}" data-method='show'><button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-1 px-2 border border-white-500 rounded">Detalles</button> </a>
+                                    @auth
+                                    @unless(Auth::user()->role=='usuario' || $evento->user->id !=Auth::user()->id )
+                                    <a href="/eventos/{{ $evento->id }}/edit" data-method='edit'><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-white-500 rounded">Editar</button></a>
+                                    <a href="/eventos/{{$evento->id}}" data-method='delete'><button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-white-500 rounded">Eliminar</button></a>
+                                    @endif
+                                    @endauth
+                                </td>
+
+                            </tr>
+
                             @endforeach
                         </tbody>
                     </table>
